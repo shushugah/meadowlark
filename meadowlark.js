70d4d4d4e8ef05f83b4739c37f6f6abe0579e4b0
Yonatan Miller <brombacher1@gmail.com>
2015-02-20 18:20:39 -0500

    set handle-bars as default views templating engine, and some routes

--- meadowlark.js
+++ meadowlark.js
@@ -1,8 +1,29 @@
 var express = require("express");
-var app = exoress();
+var app = express();
+
+//this is an html templating engine
+var handlebars = require("express-handlebars")
+	.create({defaultLayout:"main"});
+
+app.engine("handlebars", handlebars.engine);	
+
+app.set("view engine", "handlebars");
 
 app.set("port", process.env.PORT || 3000);
 
+// get method, defines routes for us, in this case: "/"
+app.get("/", function(req, res){
+	res.type("text/plain");
+	//res.status defaults to 200, not needed :)
+	res.send("Meadow Lark Travel");
+});
+
+app.get("/about", function(req, res){
+	res.type("text/plain");
+	res.send("About Meadow Lark Travel");
+});
+
+//404 page, for when invalid resource requested.
 app.use(function(req,res){
 	res.type("text/plain");
 	res.status(404);
